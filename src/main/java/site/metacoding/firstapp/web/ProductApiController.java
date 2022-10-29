416d0c2319490f7ac549215b7f2d34a84df17afe
s2staree <s2staree@gmail.com>
2022-10-29 15:19:09 +0900

    05_ 기능 - deleteById / update (RestController) 구현 완료

--- src/main/java/site/metacoding/firstapp/web/ProductApiController.java
+++ src/main/java/site/metacoding/firstapp/web/ProductApiController.java
@@ -2,9 +2,11 @@ package site.metacoding.firstapp.web;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,5 +36,17 @@ public class ProductApiController {
 	public int insert(@RequestBody Product product) {
 		int productInsert = productDao.insert(product);
 		return productInsert;
+	}
+	
+	@DeleteMapping("/product/{productId}/delete")
+	public int deleteById(@PathVariable Integer productId) {
+		int productDelete = productDao.deleteById(productId);
+		return productDelete;
+	}
+	
+	@PutMapping("/product/edit")
+	public int update(@RequestBody Product product) {
+		int productEdit = productDao.update(product);
+		return productEdit;
 	}
 }
--- src/main/java/site/metacoding/firstapp/web/TestApiController.java
+++ src/main/java/site/metacoding/firstapp/web/TestApiController.java
@@ -2,10 +2,13 @@ package site.metacoding.firstapp.web;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -46,4 +49,28 @@ public class TestApiController {
 		return productList;
 	}
 	
+	@DeleteMapping("/test/api/product/{productId}/delete")
+    public int delete(@PathVariable Integer productId) {
+		int productDelete = productDao.deleteById(productId);
+		return productDelete;
+	}
+	
+	/*
+	// put -> id로 찾아서 id로 찾은 객체의 값을 변경함 (get+post개념)
+	// get -> id를 사용해서 해당 id의 값을 가져옴
+	// post -> 객체에 값을 담아서 보냄 (db로~)
+	@PutMapping("/test/api/product/{productId}/edit")
+	public int update(@PathVariable Integer productId, @RequestBody Product product) {
+		int productEdit = productDao.update(product);
+		return productEdit;
+	}
+	*/
+	
+	// mapper(product.xml)의 update에 productId를 찾아가는 쿼리가 이미 짜져 있어서
+	// 굳이 PutMapping 주소에 {productId} 넣을 필요 없음 -> @PathVariable 쓸 필요 없음!
+	@PutMapping("/test/api/product")
+	public int update(@RequestBody Product product) {
+		int productEdit = productDao.update(product);
+		return productEdit;
+	}
 }
