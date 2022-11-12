a6e1057e5022b2e8498beb4c882c315189397872
s2staree <s2staree@gmail.com>
2022-11-12 16:41:19 +0900

    02_ 상품등록하기 기능 완료

--- src/main/java/site/metacoding/firstapp/web/ProductController.java
+++ src/main/java/site/metacoding/firstapp/web/ProductController.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.firstapp.domain.Product;
+import site.metacoding.firstapp.domain.ProductDao;
+
+@RequiredArgsConstructor	// 밑의 코드에서 선언한 코드를 new해서 안 불러도 되게 해주는 어노테이션.
 @Controller
 public class ProductController {
+	
+	private final ProductDao productDao;	// 선언. @RequiredArgsConstructor 과 함께 씀.
     
     @GetMapping({"/product/{productId}", "/product/{productId}/detail"})
     public String findById() {
@@ -17,13 +24,16 @@ public class ProductController {
         return "product/list";
     }
     
+    // 상품등록하기 Form
     @GetMapping("/product/add")
     public String insertForm() {
         return "product/add";
     }
     
+    // 상품등록하기
     @PostMapping("/product/add")
-    public String insert() {
+    public String insert(Product product) {
+    	productDao.insert(product);
         return "product/add";
     }
     
