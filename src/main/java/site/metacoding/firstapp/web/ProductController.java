4212b7c64a4408710bcecb66e37ac01becaa2e0c
s2staree <s2staree@gmail.com>
2022-11-12 17:26:25 +0900

    04_ 상품상세보기 기능 완료

--- src/main/java/site/metacoding/firstapp/web/ProductController.java
+++ src/main/java/site/metacoding/firstapp/web/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -17,14 +18,17 @@ public class ProductController {
 	
 	private final ProductDao productDao;	// 선언. @RequiredArgsConstructor 과 함께 씀.
     
+	// 상품상세보기
     @GetMapping({"/product/{productId}", "/product/{productId}/detail"})
-    public String findById() {
+    public String findById(@PathVariable Integer productId, Model model) { // @PathVariable: 매핑주소에서 id값을 찾을때 매개변수에 붙여줌.
+    	Product productPS = productDao.findById(productId);	// 영속화 되어있는 값을 가져올 때 PS를 붙임.
+    	model.addAttribute("detail", productPS);
         return "product/detail";
     }
     
 	// 상품목록보기
     @GetMapping({"/product", "/"})
-    public String findAll(Model model) { // Model: 페이지(jsp) view로 가져오기 위해서 사용. Request(요청)같은 기능.
+    public String findAll(Model model) { // Model model: 페이지(jsp) view로 가져오기 위해서 사용. Request(요청)같은 기능.
     	List<Product> productPS = productDao.findAll();
     	model.addAttribute("list", productPS);
         return "product/list";
