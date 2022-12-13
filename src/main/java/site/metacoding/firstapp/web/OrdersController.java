ba558d59e2ee80a77fb64208074a674ebbcec141
s2staree <s2staree@gmail.com>
2022-12-13 17:41:41 +0900

    21_ Orders insert 구매하기 (post - form 태그) : 주문목록 추가 기능 완료 (user 연결 X)

--- src/main/java/site/metacoding/firstapp/web/OrdersController.java
+++ src/main/java/site/metacoding/firstapp/web/OrdersController.java
@@ -2,20 +2,28 @@ package site.metacoding.firstapp.web;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.firstapp.domain.orders.Orders;
+import site.metacoding.firstapp.domain.orders.OrdersDao;
 import site.metacoding.firstapp.domain.product.Product;
 import site.metacoding.firstapp.domain.product.ProductDao;
+import site.metacoding.firstapp.domain.user.User;
 
 @RequiredArgsConstructor // 밑의 코드에서 선언한 코드를 new해서 안 불러도 되게 해주는 어노테이션.
 @Controller
 public class OrdersController {
 
 	private final ProductDao productDao; // 선언. @RequiredArgsConstructor 과 함께 씀.
+	private final OrdersDao ordersDao;
+	private final HttpSession session;
 
 	// 메인페이지 (유저-상품목록보기)
 	@GetMapping({ "/product", "/home", "/" })
@@ -35,6 +43,29 @@ public class OrdersController {
 													// productQty productName 이런식으로 있으면 덜주는건 괜찮은데 더 주는건 못참는 착한 노조라고 생각하자
 													// 이름이 달라져도 안됨 5만원 주면 일하는 앤데 6만원 주면 안되는거야!
 		return "user/product/detail";
+	}
+
+	// 구매하기 (해당 상품 재고 -1)
+	@PostMapping("/product/{productId}/buy")
+	public String buy(@PathVariable Integer productId, Orders orders) { // 나 Orders 타입의 애들거 안받으면 일 안할거야 더줘도 안하고 덜주면 일할게
+		// 디버깅 코드 잘 기억해야함!
+		// .get 을 잘 활용하자! getter 달려 있으면 다 가져올 수 있음
+		// .set은 변경시키겠다는 의미!
+		System.out.println("디버그 주문상품명" + orders.getOrderProductName()); // 주문상품
+		System.out.println("디버그 주문상품명" + orders.getOrderProductPrice()); // 주문가격
+		System.out.println("디버그 주문상품명" + orders.getOrderProductQty()); // 주문수량
+
+		// 구매한 유저의 로그인 정보를 가져옴.
+		User principal = (User) session.getAttribute("principal"); // 로그인 정보 가져오는 코드! 없으면 터짐
+
+		// 상품Id로 해당 상품 찾아서 상품DB 담음.
+		Product productPS = productDao.findById(productId);
+
+		// 주문DB에 주문 추가.
+		ordersDao.insert(orders); // orders.getUserId() == Integer userId
+
+		return "redirect:/";
+
 	}
 
 }
